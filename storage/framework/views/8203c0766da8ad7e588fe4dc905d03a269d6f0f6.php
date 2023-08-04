505e05821fe5c9ec7e4a3d47b4f439ce26856fb7
Youssefshreim7 <youssefshreim78@icloud.com>
2023-08-04 21:51:53 +0300

    Caches

---
+++ storage/framework/views/8203c0766da8ad7e588fe4dc905d03a269d6f0f6.php
@@ -0,0 +1,26 @@
+    <!-- Welcome Area Starts -->
+    <section id="about" class="flex flex-wrap item-center justify-center lg-p-0 lg:justify-between lg:p-0 lg:pt-28 max-w-[960px] mx-auto p-5">
+      <div class="basis-[500px] grow-0 pr-5">
+        <img class="h-auto w-full" src="assets/images/welcome-bg.png" alt="welcome food image" />
+      </div>
+
+      <div class="basis-[400px] grow max-w-[650px] p-5">
+        <h3 class="font-bold font-cursive-merie text-4xl">
+          <span class="text-amber-400 leading-snug">welcome</span> <br /><span class=" leading-normal">to Resto App</span>
+        </h3>
+        <p class="pt-4 text-[14px] font-sans-lato text-slate-600 leading-relaxed">
+            Welcome to our Resto App!
+
+            Indulge your taste buds with a delightful culinary experience at our restaurant. We take pride in serving a delectable array of dishes crafted with love and passion by our skilled chefs. From scrumptious starters to mouthwatering mains and irresistible desserts, our menu promises to satisfy every craving and elevate your dining experience.
+
+            Don't miss out on our special events and promotions, designed to add an extra dose of excitement to your dining experience. Stay connected with us through our newsletter and social media channels to be the first to know about our latest offerings.
+
+
+          <span class="block mt-3"></span>
+            Experience the essence of culinary excellence at Resto App. We look forward to welcoming you and creating lasting memories as we embark on a gastronomic journey together. Bon appétit!
+        </p>
+        <a href="#" class="inline-block rounded-sm shadow-md mt-7 px-5 py-3 bg-lime-400 uppercase font-sans-monts text-xs font-bold text-slate-900 border border-transparent transition ease-in-out duration-300 hover:-translate-y-1 hover:scale-110 hover:bg-transparent hover:border-lime-400">book a table</a>
+      </div>
+    </section>
+    <!-- Welcome Area End -->
+<?php /**PATH D:\restoapp\resources\views/home/partials/welcome.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/84ffcf9f89c83315ca955973fa420e66b14a5b20.php
@@ -0,0 +1,64 @@
+	<!-- Footer Area Starts -->
+	<footer id="contact" class="footer-area bg-slate-800 text-white font-sans-lato">
+	  <div class="flex flex-wrap footer-widget justify-between p-3 py-14 max-w-[960px] mx-auto section-padding">
+          <a href="/"><img class="w-36 h-auto" src="assets/images/logo/liban.png" style="width: 100px;height: 100px" alt="" /></a>
+	    <div class="basis-[300px] grow shrink p-5 pr-9 single-widget single-widget1">
+	      <p class="mt-3 leading-normal">
+              At our food haven, we believe in celebrating the beauty of simplicity.
+              Our dishes are thoughtfully curated to highlight the natural essence of each ingredient, allowing their flavors to shine through and tell their own stories.
+	      </p>
+	    </div>
+	    <div class="single-widget single-widget2 leading-loose p-5 pr-7">
+	      <h5 class="mb-2 font-bold font-cursive-merie text-lg capitalize text-amber-400">Contact Us</h5>
+	      <div class="flex items-start">
+	        <i class="fa fa-map-marker pr-3 pl-1 pt-2.5"></i>
+	        <p>
+	          SOME STREET LEBANON, <br />
+	          BEIRUT,LEBANON
+	        </p>
+	      </div>
+	      <div class="flex">
+	        <i class="fa fa-phone pr-3 pl-1  pt-2.5"></i>
+	        <p>01 000 000FF</p>
+	      </div>
+	      <div class="flex">
+	        <i class="fa fa-envelope-o pr-3 pl-1 pt-2.5"></i>
+	        <p>support@restoapp.com</p>
+	      </div>
+	    </div>
+	    <div class="single-widget single-widget3 leading-loose p-5 justify-self-end">
+	      <h5 class="mb-2 font-bold font-cursive-merie text-lg capitalize text-amber-400">OPENING HOURS</h5>
+	      <p>From: 12 PM till 12 AM</p>
+	    </div>
+	  </div>
+	  <div class="footer-copyright py-3 text-white bg-slate-900">
+	    <div class="flex flex-wrap items-center max-w-[960px] mx-auto px-3 justify-between">
+	      <span class="my-3 mx-5 leading-normal">
+	        Copyright &copy;
+	        <script>
+	          document.write(new Date().getFullYear());
+	        </script> @ Resto App || Design inspired by Resto App
+	      </span>
+	      <ul class="social-icons flex flex-wrap my-3 mx-5 transition ease-in-out duration-300">
+	        <li class="mr-4 my-1">Follow Us:</li>
+	        <li class="mr-3 my-1 cursor-pointer border border-transparent hover:border-white hover:rounded-[50%] px-1">
+	          <a href="#" class="cursor-pointer"><i class="fa-brands fa-facebook"></i></a>
+	        </li>
+	        <li class="mr-3 my-1 cursor-pointer border border-transparent hover:border-white hover:rounded-[50%] px-1">
+	          <a href="#" class="cursor-pointer"><i class="fa fa-twitter"></i></a>
+	        </li>
+	        <li class="mr-3 my-1 cursor-pointer border border-transparent hover:border-white hover:rounded-[50%] px-1">
+	          <a href="#" class="cursor-pointer"><i class="fa-brands fa-google-plus"></i></a>
+	        </li>
+	        <li class="mr-3 my-1 cursor-pointer border border-transparent hover:border-white hover:rounded-[50%] px-1">
+	          <a href="#" class="cursor-pointer"><i class="fa-brands fa-pinterest"></i></a>
+	        </li>
+	        <li class="my-1 cursor-pointer border border-transparent hover:border-white hover:rounded-[50%] px-1">
+	          <a href="#" class="cursor-pointer"><i class="fa fa-instagram"></i></a>
+	        </li>
+	      </ul>
+	    </div>
+	  </div>
+	</footer>
+	<!-- Footer Area End -->
+<?php /**PATH D:\restoapp\resources\views/home/partials/footer.blade.php ENDPATH**/ ?>
